ad20d98ce75393e98449316ba6104e3250a47124
Max Lyovin <lyovin.maxim@gmail.com>
2020-12-20 19:27:13 +0100

    add Video resize and smoothy scroll functionality

--- main.js
+++ main.js
@@ -5,4 +5,54 @@ const showMenu = () => {
     menu.classList.toggle('active');
 }
 
-menuButton.addEventListener('click', showMenu)
+menuButton.addEventListener('click', showMenu);
+
+
+
+/* ##########################
+smooth scroll
+######################### */
+
+const navigationLinks = [...document.querySelectorAll('nav a')];
+navigationLinks.forEach(a => {
+    a.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    })
+});
+
+
+
+
+/* ##########################
+VIDEO
+######################### */
+
+
+const video = document.querySelector('iframe');    
+
+
+const setVideoSize = (video) => {    
+    const heightProportion = window.innerWidth / window.innerHeight;
+    const parent = video.parentElement;
+    const parentWidth = parent.offsetWidth;
+
+    video.width = parentWidth;
+    if (window.innerWidth >= window.innerHeight) {        
+        video.height = video.width / heightProportion;
+    } else {
+        video.height = video.width;
+    }
+
+    window.addEventListener('resize', () => {
+        setVideoSize(video);
+    })
+}
+
+setVideoSize(video);
+
+
